5175544017b4c039162c81a5ee33d4109dde37fe
Tyler D <tyler@tdaigle.com>
2022-12-23 14:31:56 -0500

    input and commands working

--- day7.js
+++ day7.js
@@ -1,14 +1,66 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+const promises_1 = require("fs/promises");
 const tree_1 = require("./tree");
-// function executeCommand(command: "cd" | "ls", filesystem: Tree) {
-//   if (command === "cd") {
-//   }
-// }
-const fileSystem = new tree_1.Tree();
-fileSystem.createDirectory("/", "first");
-const dir = fileSystem.createDirectory("first", "second");
-console.log("Created: ", dir === null || dir === void 0 ? void 0 : dir.data.name);
-const parent = fileSystem.getParentDirectory("second");
-console.log(parent === null || parent === void 0 ? void 0 : parent.data.name);
+function loadInput(filename) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const input = yield (0, promises_1.readFile)(filename, { encoding: "utf-8" });
+        return input;
+    });
+}
+function executeCD(command, currentDirName, fileSystem) {
+    const [cmd, opt] = command.split(" ");
+    // make sure not to create the root directory again - the constructor already does it.
+    if (opt === "/") {
+        return fileSystem.getRoot();
+    }
+    // when we change to a new directory we should actually just create it
+    return fileSystem.createDirectory(currentDirName, opt);
+}
+(function main() {
+    return __awaiter(this, void 0, void 0, function* () {
+        const fileSystem = new tree_1.Tree();
+        fileSystem.createDirectory("/", "first");
+        const input = (yield loadInput("test-input.txt")).split("\n");
+        let currentDirName = "/";
+        input.forEach(line => {
+            const command = line.split(" ");
+            if (command[0] === "$") {
+                switch (command[1]) {
+                    case "cd":
+                        console.log("Changing Directory");
+                        executeCD(line.substring(1), currentDirName, fileSystem);
+                        currentDirName = command[2];
+                        break;
+                    case "ls":
+                        // we can kind of ignore this?
+                        console.log("Listing files");
+                        break;
+                }
+            }
+            else {
+                if (command[0] === "dir") {
+                    console.log("Creating a new directory:", command[1]);
+                    fileSystem.createDirectory(currentDirName, command[1]);
+                }
+                else {
+                    // it's a file 
+                    console.log("Adding file:", command[1]);
+                    fileSystem.addFile(currentDirName, command[1], parseInt(command[0]));
+                }
+            }
+        });
+        const files = fileSystem.getFiles("d");
+        files.forEach(file => console.log(`${file.name} : ${file.size}`));
+    });
+})();
 //# sourceMappingURL=day7.js.map
--- day7.ts
+++ day7.ts
@@ -1,16 +1,55 @@
+import { readFile } from "fs/promises";
 import { Tree, Leaf, ItemType } from "./tree";
 
-// function executeCommand(command: "cd" | "ls", filesystem: Tree) {
-//   if (command === "cd") {
+async function loadInput(filename: string) {
+  const input = await readFile(filename, { encoding: "utf-8" });
+  return input;
+}
 
-//   }
+function executeCD(command: string, currentDirName: string, fileSystem: Tree): Leaf | null {
+  const [cmd, opt] = command.split(" ");
 
+  // make sure not to create the root directory again - the constructor already does it.
+  if (opt === "/") {
+    return fileSystem.getRoot();
+  }
+  // when we change to a new directory we should actually just create it
+  return fileSystem.createDirectory(currentDirName, opt);
+}
 
-// }
+(async function main() {
+  const fileSystem = new Tree();
+  fileSystem.createDirectory("/", "first");
 
-const fileSystem = new Tree();
-fileSystem.createDirectory("/", "first");
-const dir = fileSystem.createDirectory("first", "second");
-console.log("Created: ", dir?.data.name);
-const parent = fileSystem.getParentDirectory("second");
-console.log(parent?.data.name);
+  const input = (await loadInput("test-input.txt")).split("\n");
+  let currentDirName = "/";
+
+  input.forEach(line => {
+    const command = line.split(" ");
+    if (command[0] === "$") {
+      switch (command[1]) {
+        case "cd":
+          console.log("Changing Directory");
+          executeCD(line.substring(1), currentDirName, fileSystem);
+          currentDirName = command[2];
+          break;
+        case "ls":
+          // we can kind of ignore this?
+          console.log("Listing files");
+          break;
+      }
+    } else {
+      if (command[0] === "dir") {
+        console.log("Creating a new directory:", command[1]);
+        fileSystem.createDirectory(currentDirName, command[1]);
+      } else {
+        // it's a file 
+        console.log("Adding file:", command[1]);
+        fileSystem.addFile(currentDirName, command[1], parseInt(command[0]));
+      }
+    }
+  });
+
+  const files = fileSystem.getFiles("d");
+  files.forEach(file => console.log(`${file.name} : ${file.size}`));
+})();
